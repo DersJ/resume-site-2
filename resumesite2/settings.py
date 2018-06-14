7f2cbe83cc3e6290fbaccbcada7a36ac63d167a5
Anders Juengst <dersj@live.unc.edu>
2018-06-14 00:19:18 -0400

    fixed allowed-hosts

--- resumesite2/settings.py
+++ resumesite2/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,13 +22,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lv7hzwobw_6%+57y1(0n@js!**=ghmqnn3t2_n4@v9jofvnq)e'
+SECRET_KEY = config('SECRET_KEY')
 #SECRET_KEY = os.environ.get
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['resume-site-2.herokuapp.com',
+    ]
 
 
 # Application definition
@@ -77,10 +80,9 @@ WSGI_APPLICATION = 'resumesite2.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
