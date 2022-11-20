787a255c488ac8638fe5c686a50f6714f59d7a08
Anders Juengst <anders.juengst@teradata.com>
2022-11-20 09:08:21 -0500

    light theme (#22)
    * light theme
    
    * fixes

---
+++ static/js/themeToggle.js
@@ -0,0 +1,11 @@
+function toggleDarkmode() {
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+        localStorage.setItem("dark-mode", "disabled");
+    } else {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+        localStorage.setItem("dark-mode", "enabled");
+    }
+}
