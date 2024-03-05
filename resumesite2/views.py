d1c7ad2e920a78f1668f406554c9ab7563f9d21f
Anders Juengst <anders@unchained.com>
2024-03-04 22:04:13 -0500

    update nostr.json

--- resumesite2/views.py
+++ resumesite2/views.py
@@ -4,7 +4,7 @@ import json
 
 @require_GET
 def nostr(request):
-    response = HttpResponse(json.dumps({ "names": { "anders": "d0ea1c340ebf6464809e91a3a7e5133794c5900a664243442672fea59c84dc37"}}), content_type="application/json")
+    response = HttpResponse(json.dumps({ "names": { "_": "d0ea1c340ebf6464809e91a3a7e5133794c5900a664243442672fea59c84dc37"}}), content_type="application/json")
     response["Access-Control-Allow-Origin"] = "*"
     response["Access-Control-Allow-Methods"] = "GET"
     return response
